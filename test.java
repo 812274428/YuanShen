82d833ee4afe3964219fa528e4faa035f5bba438
812274428 <49949641+812274428@users.noreply.github.com>
2023-03-27 10:14:16 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,11 @@
+package com.patchUpdate.update.coreupdate.service;
+
+import java.util.Date;
+
+public class test {
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		System.out.printf(date+":水经验");
+	}
+}
