2cdc48b98dcdbdbbd6d89a5fcc590df2d766d2cd
812274428 <49949641+812274428@users.noreply.github.com>
2023-03-29 09:06:28 +0800

    Add files via upload

---
+++ ref.java
@@ -0,0 +1,14 @@
+package com.patchUpdate.update.coreupdate.service;
+
+import java.util.Date;
+
+public class java {
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<10){
+			System.out.printf("今日第"+i+"次水经验");
+			i++;
+		}
+		
+	}
+}
